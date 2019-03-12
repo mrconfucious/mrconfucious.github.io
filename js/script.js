3befed75a9d1591a06079b3949fb231eb0a3fdcc
DESKTOP-CNA01RA\Михаил <misha.gnom@gmail.com>
2019-03-12 10:43:21 +0200

    4 grid view`s added

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function myFunction_one() {
+    var element = document.getElementById("wrapperContentID");
+    element.className ="wrapperContent";
+   
+  }
+
+
+  function myFunction_two() {
+    var element = document.getElementById("wrapperContentID");
+    element.className ="wrapperContent2";
+   
+   
+  }
+
+  
+  function myFunction_tree() {
+    var element = document.getElementById("module2_grid");
+    element.className ="module2_grid2";
+    var element2 = document.getElementById("filter_grid2");
+element2.className = "filter_grid2";
+   
+  }
+
+  function myFunction_four() {
+    var element = document.getElementById("module2_grid");
+    element.className ="module2";
+    var element2 = document.getElementById("filter_grid2");
+element2.className = "filter";
+   
+  }
